7ec2f005c17b597ad3c675178b09e6045ba7a249
Queen-Liesse <kayitare.03.11@gmail.com>
2022-08-24 10:08:05 +0200

    add course

--- src/component/class.js
+++ src/component/class.js
@@ -1,16 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import "./class.css";
 import Classes from "../assets/constant/class.json";
 import { Link } from "react-router-dom";
+import Course from "../views/course";
+import { Modal } from "antd";
 
 const Class = () => {
+  const [show, setShow] = useState(false);
+  const [singleData, setSingleData] = useState({});
   return (
     <div className="class-container" id="class">
       <h1 className="class-heading">Classes</h1>
       <h3 className="class-subheading">Classes Courses and Specializations</h3>
       <div className="classe">
         {Classes.map((data) => (
-          <Link className="class-card" to="/courses">
+          <Link
+            className="class-card"
+            to="#"
+            onClick={() => {
+              setShow(true);
+              setSingleData(data);
+            }}
+          >
             <>
               <div className="imgz">
                 <img className="class-img" src={data.classeImage} />
@@ -29,6 +40,18 @@ const Class = () => {
           </Link>
         ))}
       </div>
+      <Modal
+        width="100%"
+        onOk={() => setShow(false)}
+        onCancel={() => setShow(false)}
+        visible={show}
+        onClose={() => {
+          setShow(false);
+        }}
+        footer={null}
+      >
+        <Course data={singleData} />
+      </Modal>
     </div>
   );
 };
--- src/views/course.js
+++ src/views/course.js
@@ -5,42 +5,16 @@ import Classes from "../assets/constant/class.json";
 import { Link } from "react-router-dom";
 import Nav from "../component/nav";
 
-const Class = () => {
+const Class = ({ data }) => {
   return (
     <>
       <Nav />
       <div className="class-container" id="class">
         <h1 className="class-heading">Courses</h1>
         <h3 className="class-subheading">Courses Belong to this Class</h3>
-
-        {Classes.map((data) => (
-          <p>
-            {data.course.map((course) => (
-              <p>{course.courseName}</p>
-            ))}
-          </p>
+        {data?.course?.map((course) => (
+          <p>{course?.courseName}</p>
         ))}
-        {/* <div className="classe">
-        {Classes.map((data) => (
-          <Link className="class-card" to="/courses">
-            <>
-              <div className="imgz">
-                <img className="class-img" src={data.classeImage} />
-                <img className="class-icon" src={data.classIcon} />
-              </div>
-
-              <div className="class-name-cont">
-                <h3 className="class-name">
-                  {" "}
-                  Class:{data.className}
-                  <span>{data.classComp}</span>
-                </h3>
-                <h3 className="course-name">{data.course.courseName}</h3>
-              </div>
-            </>
-          </Link>
-        ))}
-      </div> */}
       </div>
     </>
   );
