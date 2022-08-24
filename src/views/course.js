50cf2d3f2353c9e921afa30e8df9fcd505a439d8
Queen-Liesse <kayitare.03.11@gmail.com>
2022-08-24 11:56:14 +0200

    course style

--- src/views/course.js
+++ src/views/course.js
@@ -5,6 +5,8 @@ import Classes from "../assets/constant/class.json";
 import { Link } from "react-router-dom";
 import Nav from "../component/nav";
 import "../component/class.css";
+import Image from "rc-image";
+import "./course.css";
 
 const Class = ({ data }) => {
   return (
@@ -15,7 +17,7 @@ const Class = ({ data }) => {
         <h3 className="class-subheading">Courses Belong to this Class</h3>
         <div className="classe">
           {data?.course?.map((course) => (
-            <Link
+            <div
               className="class-card"
               to="#"
               // onClick={() => {
@@ -24,11 +26,29 @@ const Class = ({ data }) => {
               // }}
             >
               <div className="imgz">
-                <img className="course-imges " src={course?.courseImage} />
+                <img className="class-img " src={course?.courseImage} />
                 {/* <img className="class-icon" src={data.classIcon} /> */}
               </div>
-              <p>{course?.courseName}</p>
-            </Link>
+
+              <h3 className="courseName"> {course?.courseName}</h3>
+
+              <div className="courseCreator">
+                <h5>CreatedBy</h5>
+                <p>{course?.createdBy}</p>
+              </div>
+              <div className="btn-course">
+                <a
+                  href="#"
+                  className="course-btn"
+                  // onClick={() => {
+                  //   setSingleData(tour);
+                  //   setModalVisible(true);
+                  // }}
+                >
+                  Start
+                </a>
+              </div>
+            </div>
           ))}
         </div>
       </div>
