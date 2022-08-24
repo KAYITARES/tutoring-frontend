f428b581dd0fff821118d0da0ddda54188dcf109
Queen-Liesse <kayitare.03.11@gmail.com>
2022-08-24 15:51:47 +0200

    add

--- src/views/singleCourse.js
+++ src/views/singleCourse.js
@@ -1,11 +1,20 @@
 import React from "react";
 import Nav from "../component/nav";
+import "./singleCourse.css";
 
 const SingleCourse = ({ data }) => {
   return (
-    <div>
+    <div className="class-container">
       <Nav />
-      <p>{data.duration}</p>
+      <h1 className="class-heading">{data.courseName}</h1>
+      <h3 className="class-subheading marginBottom-0 paddinBottom-0">
+        {data.courseTitle}
+      </h3>
+      <div className="duration">
+        <h5 className="duration-title">Duration</h5>
+        <p className="course-duration">{data.duration}</p>
+      </div>
+      <p>{data.content}</p>
     </div>
   );
 };
